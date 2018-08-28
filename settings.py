2ac9c6b5dc1ae04450c8ff4ca4a62faa408e9f96
Sergey Talash <icelangolier@gmail.com>
2018-08-28 09:27:26 +0000

    Upload New File

---
+++ settings.py
@@ -0,0 +1,104 @@
+{
+    "calibration": {
+        "nm(pix)": {
+            "S": [
+                "3.23553847*10**-8", 
+                "0.0393587546", 
+                "272.703981"
+            ], 
+            "Z": [
+                "7.454057229*10**-8", 
+                "0.03923741803", 
+                "273.905099"
+            ]
+        }, 
+        "pix_interval": 25, 
+        "points": {
+            "Fraunhofer_pair": [
+                393.37, 
+                396.78
+            ], 
+            "cloud_pair_1": [
+                332, 
+                352
+            ], 
+            "cloud_pair_2": [
+                340, 
+                360
+            ], 
+            "o3_pair_1": [
+                312, 
+                332
+            ], 
+            "o3_pair_2": [
+                317, 
+                340
+            ]
+        }, 
+        "polynom": {
+            "degree": 3, 
+            "mu_end": 12, 
+            "mu_intervals": [
+                3, 
+                5
+            ], 
+            "mu_start": 1, 
+            "mu_step": 0.05
+        }, 
+        "sigma_count": 2, 
+        "sko_D": 100, 
+        "sko_ZaD": 100, 
+        "uva_koef": 1, 
+        "uvb_koef": 1, 
+        "uve_koef": 1
+    }, 
+    "calibration2": {
+        "kzLarger2": [
+            1.3834, 
+            -0.1423, 
+            0.0147
+        ], 
+        "kzLess2": [
+            1.3359, 
+            -0.0783, 
+            -0.005
+        ], 
+        "kz_obl": [
+            1.4773, 
+            -1.7878, 
+            1.3051
+        ], 
+        "pix+-": 2
+    }, 
+    "channel_names": {
+        "D": "D", 
+        "S": "S", 
+        "Z": "Z"
+    }, 
+    "device": {
+        "accummulate": 1, 
+        "amplitude_max": 3200, 
+        "amplitude_min": 2800, 
+        "auto_expo_max": 4000, 
+        "auto_expo_min": 50, 
+        "auto_exposition": 1, 
+        "channel": "ZS", 
+        "graduation_expo": 4000, 
+        "id": 7, 
+        "manual_expo": [
+            4000, 
+            4001, 
+            4002, 
+            4005
+        ]
+    }, 
+    "station": {
+        "id": "26077", 
+        "interval": 5, 
+        "latitude": 59.57, 
+        "longitude": -30.42, 
+        "sun_height_min": 5, 
+        "timezone": "+3"
+    }, 
+    "version": "2.0"
+}
