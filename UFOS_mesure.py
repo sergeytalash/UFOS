ac7f1246f1f5a163ee9df7c9d41b49d09cf83c99
Sergey Talash <icelangolier@gmail.com>
2018-09-04 11:52:38 +0000

    Replace UFOS_mesure.py

--- UFOS_mesure.py
+++ UFOS_mesure.py
@@ -19,8 +19,8 @@ import procedures
 if __name__ == '__main__':
     date_time = datetime.datetime.now()
     logging.basicConfig(filename='logs/ufos.log',level=logging.INFO)
-    mu,amas,hs = procedures.sunheight(59.57,-30.42,date_time,+0)
-    print('mu= {}\namas= {}\nsunheight= {}\n================'.format(mu,amas,hs))
+##    mu,amas,hs = procedures.sunheight(59.57,-30.42,date_time,+0)
+##    print('mu= {}\namas= {}\nsunheight= {}\n================'.format(mu,amas,hs))
     version = '2.0'
     procedures.check_sun_and_mesure().start()
 
