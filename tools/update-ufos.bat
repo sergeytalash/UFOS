c5585e00d8b5e8f70d6966d1688b2b30b073c548
sergey.talash <lasmor@inbox.ru>
2019-06-22 18:25:00 +0300

    update tool added

---
+++ tools/update-ufos.bat
@@ -0,0 +1,9 @@
+d:
+cd d:\ufos
+git fetch --all
+git checkout origin/master -- procedures.py
+git checkout origin/master -- connect.ini
+git checkout origin/master -- UFOS_plotter.pyw
+git checkout origin/master -- UFOS_start.pyw
+git checkout origin/master -- UFOS_mesure.py
+pause
